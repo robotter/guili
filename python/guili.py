3c82260de53a6d3b38ac62f0a60c011dba15dd1c
Benoît Ryder <benoit@ryder.fr>
2014-01-19 23:34:52 +0100

    Change ROME related stuff, reorganize GuiliServer
    ROME messages are now retrieved from Python module. wsdo_rome() is now
    undefined by default on RequestHandler (it has to be redefined in subclasses).
    
    GuiliServer is now a base class. Two subclasses have been defined:
     - TestGuiliServer with previous implementation, for tests
     - RomeClientGuiliServer which uses a ROME client

--- python/guili.py
+++ python/guili.py
@@ -10,68 +10,10 @@ import mimetypes
 import shutil
 from websocket import WebSocketServer, WebSocketRequestHandler
 from SocketServer import ThreadingMixIn
+import rome
 
 if not mimetypes.inited:
   mimetypes.init()
-
-
-class GuiliServer(ThreadingMixIn, WebSocketServer):
-  """
-  Guili application server
-
-  Attributes:
-    lock -- lock for concurrent accesses
-    requests -- set of request handlers of initialized clients
-
-  """
-
-  daemon_threads = True
-
-  def __init__(self, addr):
-    self.data = None
-    self.requests = set()
-    self.lock = threading.RLock()
-    WebSocketServer.__init__(self, addr, GuiliRequestHandler)
-    self._gen_data = self.gen_data() #XXX
-    #XXX
-    self.messages = {
-        'goto_xy': [('x', 'int16'), ('y', 'int16')],
-        'dummy': [],
-        }
-
-  def on_new_data(self, data):
-    with self.lock:
-      for r in self.requests:
-        if not r.paused:
-          r.send_event('data', {'data': data})
-
-  def on_robot_event(self, ev):
-    """Called on new event from the robot"""
-    #XXX
-    self.on_new_data(self._gen_data.next())
-
-  #XXX
-  def gen_data(self):
-    data = {
-        'robot': {
-          'x': None, 'y': None,
-          'vx': 120., 'vy': -200.,
-          'ax': 34., 'ay': 78.,
-          },
-        }
-
-    import itertools
-    import math
-    r = 600
-    N = 100
-    for i in itertools.cycle(range(N)):
-      data['robot']['x'] = r * math.cos(2*i*math.pi/N)
-      data['robot']['y'] = r * math.sin(2*i*math.pi/N)
-      yield data
-
-  def rome_goto_xy(self, x, y):
-    print "received goto_xy(%r, %r)" % (x, y)
-
 
 
 class GuiliRequestHandler(WebSocketRequestHandler):
@@ -166,7 +108,7 @@ class GuiliRequestHandler(WebSocketRequestHandler):
     try:
       getattr(self, 'wsdo_'+data['method'].replace('-', '_'))(**data['params'])
     except Exception as e:
-      self.send_event('log', {'severity': 'error', 'message': str(e)})
+      self.send_event('log', {'severity': 'error', 'message': "%s: %s" % (e.__class__.__name__, str(e))})
 
   def wsdo_init(self):
     """Initialize a client"""
@@ -180,11 +122,81 @@ class GuiliRequestHandler(WebSocketRequestHandler):
 
   def wsdo_rome(self, name, params):
     """Send a ROME message"""
-    getattr(self.server, 'rome_'+name)(**params)
+    raise NotImplementedError
 
   def wsdo_rome_messages(self):
     """Send ROME message definitions"""
-    self.send_event('messages', {'messages': self.server.messages})
+    messages = { msg.name: [(k,t.name) for k,t in msg.ptypes] for msg in rome.messages.values() }
+    self.send_event('messages', {'messages': messages})
+
+
+class GuiliServer(ThreadingMixIn, WebSocketServer):
+  """
+  Guili application server
+
+  Attributes:
+    lock -- lock for concurrent accesses
+    requests -- set of request handlers of initialized clients
+
+  """
+
+  daemon_threads = True
+  GuiliRequestHandlerClass = GuiliRequestHandler
+
+  def __init__(self, addr):
+    self.data = None
+    self.requests = set()
+    self.lock = threading.RLock()
+    WebSocketServer.__init__(self, addr, self.GuiliRequestHandlerClass)
+
+  def on_new_data(self, data):
+    with self.lock:
+      for r in self.requests:
+        if not r.paused:
+          r.send_event('data', {'data': data})
+
+  def start(self):
+    self.serve_forever()
+
+
+class TestGuiliServer(GuiliServer):
+  """
+  Dummy server, for tests
+  """
+
+  class GuiliRequestHandlerClass(GuiliRequestHandler):
+    def wsdo_rome(self, name, params):
+      print "ROME: %s %r" % (name, params)
+
+  def __init__(self, addr):
+    GuiliServer.__init__(self, addr)
+    self._gen_data = self.gen_data()
+
+  def start(self):
+    TickThread(0.1, self.on_robot_event).start()
+    GuiliServer.start(self)
+
+  def on_robot_event(self, ev):
+    """Called on new event from the robot"""
+    self.on_new_data(self._gen_data.next())
+
+  def gen_data(self):
+    data = {
+        'robot': {
+          'x': None, 'y': None,
+          'vx': 120., 'vy': -200.,
+          'ax': 34., 'ay': 78.,
+          },
+        }
+
+    import itertools
+    import math
+    r = 600
+    N = 100
+    for i in itertools.cycle(range(N)):
+      data['robot']['x'] = r * math.cos(2*i*math.pi/N)
+      data['robot']['y'] = r * math.sin(2*i*math.pi/N)
+      yield data
 
 
 class TickThread(threading.Thread):
@@ -204,6 +216,28 @@ class TickThread(threading.Thread):
       time.sleep(self.dt)
 
 
+class RomeClientGuiliServer(GuiliServer):
+
+  class GuiliRequestHandlerClass(GuiliRequestHandler):
+    def wsdo_rome(self, name, params):
+      #TODO use cb_result/cb_ack when needed
+      frame = rome.Frame(name, **params)
+      self.server.client.send(frame)
+
+  class RomeClientClass(rome.Client):
+    def on_frame(self, frame):
+      pass #TODO
+
+  def __init__(self, addr, rome_fo):
+    GuiliServer.__init__(self, addr)
+    self.client = self.RomeClientClass(rome_fo)
+    self.client.guili_server = self
+
+  def start(self):
+    self.client.start(self)
+    GuiliServer.start(self)
+
+
 
 def main():
   import argparse
@@ -218,9 +252,8 @@ def main():
     GuiliRequestHandler.files_base_path = os.path.abspath(args.web_dir)
 
   print "starting server on port %d" % args.port
-  server = GuiliServer(('', args.port))
-  TickThread(0.1, server.on_robot_event).start() #XXX
-  server.serve_forever()
+  server = TestGuiliServer(('', args.port))
+  server.start()
 
 if __name__ == '__main__':
   main()
