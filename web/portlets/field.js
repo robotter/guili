f517b2325b4e95c503b382d74b5a119f2236b0a6
Benoît Ryder <benoit@ryder.fr>
2015-05-12 12:11:52 +0200

    field: transpose robot position directly in SVG
    This allows to use our coordinate system in JS scripts.

--- web/portlets/field.js
+++ web/portlets/field.js
@@ -18,6 +18,7 @@ Portlet.register({
       // according to SVG viewBox ratio
       var viewBox = field.viewBox.baseVal;
       self.node.height(self.node.width() * viewBox.height / viewBox.width);
+      var frame = field.getElementById('reference-frame');
 
       // create SVG robot
       var svg_robot = field.createElement('use');
@@ -25,10 +26,11 @@ Portlet.register({
         'id': 'galipeur',
         'xlink:href': '#def-galipeur',
       });
-      field.appendChild(svg_robot);
+      frame.appendChild(svg_robot);
 
       // set portlet properties
       self.field = field;
+      self.frame = frame;
       self.svg_robot = svg_robot;
 
       self.bindFrame('asserv_tm_xya', self.updatePosition);
@@ -41,7 +43,7 @@ Portlet.register({
 
   updatePosition: function(params) {
     this.svg_robot.setAttributes({
-      'transform': "translate("+params.x+","+(-params.y + 1000)+") rotate("+(-params.a*180/Math.PI)+")",
+      'transform': "translate("+params.x+","+params.y+") rotate("+(params.a*180/Math.PI)+")",
     });
   },
 
