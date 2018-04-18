b1e8d19f583475aa61cdce31d64b5151f63cad5f
Benoît Ryder <benoit@ryder.fr>
2018-04-18 15:04:30 +0200

    Handle meca telemetry

--- web/portlets/meca.js
+++ web/portlets/meca.js
@@ -14,12 +14,22 @@ Portlet.register({
     var object = this.content.children('object')[0];
     object.onload = function() {
       self.cylinder = object.getSVGDocument().getElementById('cylinder');
+      self.state = object.getSVGDocument().getElementById('state')
+      self.emptying_move = object.getSVGDocument().getElementById('emptying_move')
 
-      self.bindFrame(null, 'meca_tm_state', function(robot, params) {
-        //TODO
-      });
       self.bindFrame(null, 'meca_tm_cylinder_state', function(robot, params) {
-        //TODO
+        for(var i=0; i<params.color.length; ++i) {
+          self.cylinder.getElementById('slot-'+i).setAttribute('class', 'slot color-'+params.color[i]);
+        }
+      });
+      self.bindFrame(null, 'meca_tm_cylinder_position', function(robot, params) {
+        self.cylinder.setAttribute('transform', "rotate("+(params.a*180/Math.PI)+")");
+      });
+      self.bindFrame(null, 'meca_tm_state', function(robot, params) {
+        self.state.setAttribute('class', params.state);
+      });
+      self.bindFrame(null, 'meca_tm_optimal_emptying_move', function(robot, params) {
+        self.emptying_move.setAttribute('class', params.emptying_move);
       });
     };
 
