7c3e965092af8e11d07151d9a2b3fb8c738741b0
Augustin <aboisvert@fly-n-sense.com>
2018-04-18 17:33:18 +0200

    noob lauch script updated for python3 support

--- guili.sh
+++ guili.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-PYTHONPATH=../robotter/:../eurobot/:../avarix/bootloader python python/guili.py --web-dir web/ --xbee-api /dev/ttyUSB0 1234 galipeur:20 galipette:21
+PYTHONPATH=../robotter/:../eurobot/:../avarix/bootloader python/guili.py --web-dir web/ --xbee-api /dev/ttyUSB0 1234 galipeur:20 galipette:21
