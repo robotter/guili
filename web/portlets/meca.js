b42a637eb482d133fa08279f0dfaf1fd1ae37dff
Benoît Ryder <benoit@ryder.fr>
2018-04-18 13:05:41 +0200

    Finish cylinder SVG

--- web/portlets/meca.js
+++ web/portlets/meca.js
@@ -6,14 +6,14 @@ Portlet.register({
     Portlet.prototype.init.call(this, options);
     this.node.css('width', '200px');
     this.node.resizable({ containment: 'parent', aspectRatio: true, minWidth: 100 });
-    var barillet = document.getElementById("barillet");
+    var cylinder = document.getElementById("cylinder");
 
     // wait for the SVG document to be loaded before using it
     var self = this;
     var df = $.Deferred();
     var object = this.content.children('object')[0];
     object.onload = function() {
-      self.barillet = object.getSVGDocument().getElementById('barillet');
+      self.cylinder = object.getSVGDocument().getElementById('cylinder');
 
       self.bindFrame(null, 'meca_tm_state', function(robot, params) {
         //TODO
