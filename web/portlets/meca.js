8ebf3732594b8900b2d1176d5888224f3746d974
Benoît Ryder <benoit@ryder.fr>
2018-04-19 12:45:22 +0200

    Finalize meca portlet with cylinder

--- web/portlets/meca.js
+++ web/portlets/meca.js
@@ -6,30 +6,41 @@ Portlet.register({
     Portlet.prototype.init.call(this, options);
     this.node.css('width', '200px');
     this.node.resizable({ containment: 'parent', aspectRatio: true, minWidth: 100 });
-    var cylinder = document.getElementById("cylinder");
 
     // wait for the SVG document to be loaded before using it
     var self = this;
     var df = $.Deferred();
     var object = this.content.children('object')[0];
     object.onload = function() {
-      self.cylinder = object.getSVGDocument().getElementById('cylinder');
-      self.state = object.getSVGDocument().getElementById('state')
-      self.emptying_move = object.getSVGDocument().getElementById('emptying_move')
+      var svg = object.getSVGDocument()
+      self.cylinder = svg.getElementById('cylinder');
+      self.state = svg.getElementById('state')
+      self.emptying_move = svg.getElementById('emptying_move')
+      self.rotate_cylinder = svg.getElementById('rotate-cylinder')
+      self.last_angle = 0;
 
       self.bindFrame(null, 'meca_tm_cylinder_state', function(robot, params) {
         for(var i=0; i<params.color.length; ++i) {
-          self.cylinder.getElementById('slot-'+i).setAttribute('class', 'slot color-'+params.color[i]);
+          svg.getElementById('slot-'+i).setAttribute('class', 'slot color-'+params.color[i]);
         }
       });
       self.bindFrame(null, 'meca_tm_cylinder_position', function(robot, params) {
-        self.cylinder.setAttribute('transform', "rotate("+(params.a*180/Math.PI)+")");
+        var angle = params.a * 180 / Math.PI;
+        if(angle == self.last_angle) {
+          return;
+        }
+        var da = Math.abs(angle - self.last_angle);
+        self.rotate_cylinder.setAttribute('from', self.last_angle);
+        self.rotate_cylinder.setAttribute('dur', (da / 300)+'s');
+        self.rotate_cylinder.setAttribute('to', angle);
+        self.rotate_cylinder.beginElement();
+        self.last_angle = angle;
       });
       self.bindFrame(null, 'meca_tm_state', function(robot, params) {
         self.state.setAttribute('class', params.state);
       });
       self.bindFrame(null, 'meca_tm_optimal_emptying_move', function(robot, params) {
-        self.emptying_move.setAttribute('class', params.emptying_move);
+        self.emptying_move.setAttribute('class', params.move);
       });
       df.resolve();
     };
