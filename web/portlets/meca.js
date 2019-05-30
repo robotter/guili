0b6306e5579d7691bff4261b46c6ae5dc2eb8b63
Benoît Ryder <benoit@ryder.fr>
2019-05-30 20:45:56 +0200

    Adapt meca portlet for Eurobot 2019

--- web/portlets/meca.js
+++ web/portlets/meca.js
@@ -3,44 +3,31 @@ Portlet.register('meca', 'Meca', class extends Portlet {
 
   async init(options) {
     await super.init(options);
-    this.node.style.width = '200px';
-    this.enableResize({ min_w: 100, ratio: true });
 
-    // wait for the SVG document to be loaded before using it
-    await new Promise((resolve, reject) => {
-      const object = this.content.querySelector('object');
-      object.onload = () => {
-        const svg = object.getSVGDocument()
-        this.cylinder = svg.getElementById('cylinder');
-        this.state = svg.getElementById('state')
-        this.emptying_move = svg.getElementById('emptying_move')
-        this.rotate_cylinder = svg.getElementById('rotate-cylinder')
-        this.last_angle = 0;
+    var state_to_color = {
+      busy: 'red',
+      ready: 'green',
+      ground_clear: 'orange',
+    };
 
-        this.bindFrame(null, 'meca_tm_cylinder_state', (frame) => {
-          for(const [i, color] of frame.params.color) {
-            svg.getElementById('slot-'+i).setAttribute('class', 'slot color-'+color);
-          }
-        });
-        this.bindFrame(null, 'meca_tm_cylinder_position', (frame) => {
-          const angle = frame.params.a * 180 / Math.PI;
-          if(angle == this.last_angle) {
-            return;
-          }
-          const da = Math.abs(angle - this.last_angle);
-          this.rotate_cylinder.setAttribute('from', this.last_angle);
-          this.rotate_cylinder.setAttribute('dur', (da / 300)+'s');
-          this.rotate_cylinder.setAttribute('to', angle);
-          this.rotate_cylinder.beginElement();
-          this.last_angle = angle;
-        });
-        this.bindFrame(null, 'meca_tm_state', (frame) => {
-          this.state.setAttribute('class', frame.params.state);
-        });
-        this.bindFrame(null, 'meca_tm_optimal_emptying_move', (frame) => {
-          this.emptying_move.setAttribute('class', frame.params.move);
-        });
-        resolve();
+    this.bindFrame(null, 'meca_tm_state', (frame) => {
+      const td = this.content.querySelector('td');
+      td.firstChild.style.color = state_to_color[frame.params.state];
+      console.log(td.firstChild.style.color);
+    });
+
+    this.bindFrame(null, 'meca_tm_arms_state', (frame) => {
+      const params = frame.params;
+      const tds = this.content.querySelectorAll('td');
+
+      tds[1].textContent = params.l_pos < 0 ? '✗' : params.l_pos.toFixedHtml(0);
+      for(var i = 0; i < 3; i++) {
+        tds[2+i].firstChild.style.color = params.l_atoms[i] ? 'black' : 'gray';
+      }
+
+      tds[5].textContent = params.r_pos < 0 ? '?' : params.r_pos.toFixedHtml(0);
+      for(var i = 0; i < 3; i++) {
+        tds[6+i].firstChild.style.color = params.r_atoms[i] ? 'black' : 'gray';
       }
     });
   }
