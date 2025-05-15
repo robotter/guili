d1d67c03149d3f039bfc78c596fbdfbbe17dd2e0
Augustin <augustin.boisvert@gmail.com>
2025-05-15 07:38:16 +0200

    add test arg for og's guili launch script

--- guili.sh
+++ guili.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-PYTHONPATH=../robotter/:../eurobot/:../avarix/bootloader python/guili.py --web-dir web/ --xbee-api /dev/ttyUSB0 1234 galipeur:20 galipette:21 boom:10 pano:aa
+if [ "$1" != "test" ] ; then
+    PYTHONPATH=../robotter/:../eurobot/:../avarix/bootloader python/guili.py --web-dir web/ --xbee-api /dev/ttyUSB0 1234 galipeur:20 galipette:21 boom:10 pano:aa
+  else
+    PYTHONPATH=../robotter/:../eurobot/:../avarix/bootloader python/guili.py --web-dir web/ --xbee-api /dev/ttyUSB0 1234 test:TEST
+  fi
